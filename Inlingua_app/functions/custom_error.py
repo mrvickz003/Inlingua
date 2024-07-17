02d21707c04a119bab7656b3634238abb9a3c940
Vignesh <157564464+Revaavickz003@users.noreply.github.com>
2024-07-17 20:14:39 +0530

    update on 17-7-2024

--- Inlingua_app/functions/custom_error.py
+++
@@ -1,4 +0,0 @@
-# from django.http import HttpResponseNotFound
-
-# def custom_404_view(request, exception=None):
-#     return HttpResponseNotFound('<h1>Page not found</h1>')
--- Inlingua_app/functions/user.py
+++ Inlingua_app/functions/user.py
@@ -5,14 +5,6 @@ from django.http import JsonResponse
 from django.core.paginator import Paginator
 from Inlingua_app.utils import send_welcome_email
 
-def user_page(request):
-        context = {
-                'Students':'active',
-                'all_students': StudentTable.objects.all(),
-                }
-        return render(request, "inlingua/user.html",context)
-
-
 def student_list(request):
     all_students = StudentTable.objects.all()[::-1]
     paginator = Paginator(all_students, 10)
@@ -23,15 +15,7 @@ def student_list(request):
         'all_students': page_obj, 
         'page_obj': page_obj,
     }
-    
     return render(request, 'inlingua/user.html', context)
-
-
-# def student_details(request,id):
-#     context = {
-#         'Students':'active',
-#     }
-#     return render(request, "inlingua/student_details.html",context)
 
 def addstudent(request):
     if request.method == 'POST':
@@ -80,8 +64,8 @@ def addstudent(request):
             Account_Holder_Name=account_holder_name,
             Amount_Paide=amount_paide,
             Balance_Amount=balance_amount,
-            Created_by=request.user.username,
-            Updated_by=request.user.username,
+            Created_by=request.user,
+            Updated_by=request.user,
         )
         student.save()
         send_welcome_email(student.Student_Mail_Id, student.Student_Name)
@@ -103,110 +87,3 @@ def mail(request):
 def get_levels(request, language_id):
     levels = LevelsAndHour.objects.filter(language=language_id).values('id', 'level', 'hours', 'help_text')
     return JsonResponse({'levels': list(levels)})
-
-# def profileupdate(request, id):
-#     if request.user.is_authenticated:
-#         user_id = request.user.id
-#         user = User.objects.get(id=user_id)
-#         if user.is_staff and user.is_staff and user.is_superuser:
-#             if request.method == 'POST':
-#                 profile = request.FILES.get('changeimg')
-#                 if profile:
-#                     changeprofile = User.objects.get(id = id)
-#                     changeprofile.user_img =  profile
-#                     changeprofile.save()
-#                     messages.success(request,  f"Profile picture updated successfully!")
-#                     return redirect('students')
-#                 else:
-#                     messages.error(request,  "Image field is empty! Please select an image to update your profile picture.")
-#                     return redirect('students')
-#             else:
-#                 user_detail = get_object_or_404(User, id=id)
-#                 context={'User':user,'Users':user_detail}
-#                 return render(request,'inlingua/admin-editProfile.html',context)
-#         else:
-#             messages.error(request, "You are not authorized to view this page ")
-#             return redirect('home')
-#     else:
-#         messages.info(request, "Please Login first!")
-#         return redirect('login')  
-                
-# def basic_details_update(request, id):
-#     if request.user.is_authenticated:
-#         user_id = request.user.id
-#         user = User.objects.get(id=user_id)
-#         if user.is_staff and user.is_staff and user.is_superuser:
-#             if request.method == 'POST':
-#                 studentname = request.POST.get('studentname')
-#                 mobilenumber = request.POST.get('mobilenumber')
-#                 fname = request.POST.get('fname')
-#                 lname = request.POST.get('lname')
-#                 email = request.POST.get('email')
-#                 Location = request.POST.get('Location')
-                
-#                 if studentname and mobilenumber and email and Location :
-#                     getuser = User.objects.get(id = id)
-#                     getuser.name = studentname
-#                     getuser.Mobile_Number = mobilenumber
-#                     getuser.first_name = fname
-#                     getuser.last_name = lname
-#                     getuser.Address =  Location
-#                     if getuser.email ==  email:
-#                         pass
-#                     else:
-#                         if not User.objects.filter(email=email).exists():
-#                             getuser.email = email
-#                             getuser.save()
-#                         else:
-#                             messages.warning(request,"Email already exists.")
-#                             return redirect('students')
-#                     getuser.save()
-#                     messages.success(request,  f"{studentname} Basic information details updated successfully")
-#                     return redirect('students')
-#                 else:
-#                     messages.error(request,  "All fields must be filled out correctly.")
-#                     return redirect('students')
-#             else:
-#                 user_detail = get_object_or_404(User, id=id)
-#                 context={'User':user,'Users':user_detail}
-#                 return render(request,'inlingua/admin-editProfile.html',context)
-#         else:
-#             messages.error(request, "You are not authorized to view this page ")
-#             return redirect('home')
-#     else:
-#         messages.info(request, "Please Login first!")
-#         return redirect('login')  
-    
-# def studentbatchdetals(request, id):
-#     if request.user.is_authenticated:
-#         user_id = request.user.id
-#         user = User.objects.get(id=user_id)
-#         if user.is_staff and user.is_staff and user.is_superuser:
-#             if request.method == 'POST':
-#                 batch = request.POST['Batchid']
-#                 try:
-#                     getbatch = TrainingBatches.objects.get(ID = int(batch))
-#                 except:
-#                     messages.error(request, "Invalid Batch ID")
-#                     return redirect('students')
-
-#                 if getbatch:
-#                     updatestudent = StudentDetails.objects.get(StudentID=id)
-#                     updatestudent.BatchID = getbatch
-#                     updatestudent.StudentID.updated_by =  user.name
-#                     updatestudent.StudentID.updated_date =  datetime.datetime.now()
-#                     updatestudent.save()
-#                     messages.success(request,  f"Course details updated successfully")
-#                     return redirect('students')
-#                 else:
-#                     messages.warning(request, 'Sorry Select a corrct Course')
-#                     return redirect('students')
-#             else:
-#                 messages.error(request, "You are not authorized to view this page ")
-#                 return redirect('home')
-#         else:
-#             messages.error(request, "You are not authorized to view this page ")
-#             return redirect('home')
-#     else:
-#         messages.info(request, "Please Login first!")
-#         return redirect('login')  
