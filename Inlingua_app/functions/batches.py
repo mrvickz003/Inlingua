2eb413205a2ef6ade373d66bee1d92fd5f492ff5
Vignesh <157564464+Revaavickz003@users.noreply.github.com>
2024-07-24 16:49:04 +0530

    update on 7/24/2024

--- Inlingua_app/functions/batches.py
+++ Inlingua_app/functions/batches.py
@@ -2,10 +2,23 @@ from django.shortcuts import render, redirect
 from Inlingua_app.models import *
 from django.core.paginator import Paginator
 from django.db.models import Q
+from django.http import JsonResponse
 
 def create_batch(request):
     context={
         'Dashboard':'active',
         'languages':Language.objects.all(),
+        'Batch_Preferences':batch_preferences.objects.all()
     }
     return render(request, 'inlingua/add_batchs.html', context)
+
+
+def load_levels(request):
+    language_id = request.GET.get('language_id')
+    levels = LevelsAndHour.objects.filter(language_id=language_id).values('id', 'level')
+    return JsonResponse(list(levels), safe=False)
+
+def load_time_slots(request):
+    batchpreference_id = request.GET.get('batchpreference_id')
+    time_slots = BatchTiming.objects.filter(batch_preferences=batchpreference_id).values('id', 'timing')
+    return JsonResponse(list(time_slots), safe=False)
--- Inlingua_app/functions/trainers.py
+++ Inlingua_app/functions/trainers.py
@@ -8,47 +8,80 @@ def trainers_view(request):
     trainersList = TrainerTable.objects.all()
     paginatorTrainer = Paginator(trainersList, 10)
     pageNumber = request.GET.get('page')
-    pageObj = paginatorTrainer.get_page(pageNumber)
+    pageObjTrainer = paginatorTrainer.get_page(pageNumber)
     context = {
         'Trainers': 'active',
         'trainersList': TrainerTable.objects.all(),
-        'paginatorTrainer': pageObj,
-        'PageObj': pageObj
+        'paginator': pageObjTrainer,
+        'PageObjTrainer': pageObjTrainer
     }
     return render(request, "inlingua/trainers.html", context)
 
 def add_trainers(request):
     if request.method == 'POST':
-    # Retrieve all form fields
+        # Step 1: Trainer Personal Details
         trainer_name = request.POST.get('trainer_name')
         trainer_dob = request.POST.get('trainer_dob')
-        trainer_education = request.POST.get('trainer_education')
         trainer_mail = request.POST.get('trainer_mail')
         trainer_phone = request.POST.get('trainer_phone')
-        trainer_languages = request.POST.get('trainer_languages')
         trainer_address = request.POST.get('trainer_address')
+        trainer_aadhar = request.FILES.get('trainer_aadhar')  # Use request.FILES for file fields
         trainer_photo = request.FILES.get('trainer_photo')  # Use request.FILES for file fields
-        trainer_bank_details = request.POST.get('trainer_bank')
-        trainer_aadhar = request.POST.get('trainer_aadhar')
-        trainer_role = request.POST.get('trainer_role')
 
-    # Example: Create a Trainer object and save it to the database
-        new_trainer = TrainerTable.objects.create(
-            trainer_name_mod = trainer_name,
-            trainer_dob_mod = trainer_dob,
-            trainer_education_mod = trainer_education,
-            trainer_mail_mod = trainer_mail,
-            trainer_phone_mod = trainer_phone,
-            trainer_languages_mod = trainer_languages,
-            trainer_address_mod = trainer_address,
-            trainer_photo_mod = trainer_photo,
-            trainer_bank_details_mod = trainer_bank_details,
-            trainer_aadhar_mod = trainer_aadhar,
-            trainer_role_mod = trainer_role
+        # Retrieve or create the language instance based on form input
+        trainer_languages = request.POST.get('trainer_languages')
+        trainer_language = Language.objects.get(id=trainer_languages)
+
+        # Step 2: Education Details
+        qualification = request.POST.get('education_qualification')
+        institute = request.POST.get('education_institute')
+        year_of_passing = request.POST.get('education_year_of_passing')
+
+        # Step 3: Bank Details
+        account_number = request.POST.get('bank_account_number')
+        bank_name = request.POST.get('bank_name')
+        branch = request.POST.get('bank_branch')
+        ifsc_code = request.POST.get('bank_ifsc_code')
+        passbook_front_page = request.FILES.get('bank_passbook')
+
+        # Create User table
+        New_user = User.objects.create(
+            email=trainer_mail,   
         )
+        
+        # Create a new Trainer object and save it to the database
+
+        new_trainer = TrainerTable.objects.create(
+            user = New_user,
+            trainer_name=trainer_name,
+            trainer_dob=trainer_dob,
+            trainer_mail=trainer_mail,
+            trainer_phone=trainer_phone,
+            trainer_languages=trainer_language,
+            trainer_address=trainer_address,
+            trainer_aadhar = trainer_aadhar,
+            trainer_photo=trainer_photo,
+            qualification=qualification,
+            institution=institute,
+            year_of_passing=year_of_passing,
+            bank_account_number=account_number,
+            bank_name=bank_name,
+            bank_branch=branch,
+            bank_ifsc=ifsc_code,
+            passbook=passbook_front_page,
+            
+            Created_by=request.user,
+            Created_date=dt.now()
+        )
+        New_user.username = new_trainer.Trainer_ID
+        New_user.set_password(trainer_mail)
+        New_user.save()
         new_trainer.save()
-    return render(request, 'inlingua/addtrainer.html')
-
-
+        return redirect('trainers')
+    
+    context = {
+        'All_languages': Language.objects.all()
+        }
+    return render(request, "inlingua/addtrainer.html", context)
 
 
